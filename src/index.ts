231623f101fdd1243765f0267e900f86474e78f5
debbysa <deby.silvia23@gmail.com>
2021-04-28 14:26:40 +0700

    init npm configuration

--- src/index.ts
+++ src/index.ts
@@ -5,10 +5,15 @@ import { GetAllCategory } from './classes/GetAllCategory'
 import { GetCategory } from './classes/GetCategory'
 import { GetProduct } from './classes/GetProduct'
 import { GetBalance } from './classes/GetBalance'
-import { TopUpMobile } from './classes/TopUpMobile'
-import { CheckPartnerOrder } from './classes/CheckPartnerOrder'
 import { CheckBillInfo } from './classes/CheckBillInfo'
+import { GetTelcoPromotions } from './classes/GetTelcoPromotions'
+import { GetCardData } from './classes/GetCardData'
+import { GetCardQuantity } from './classes/GetCardQuantity'
+import { CheckPartnerOrder } from './classes/CheckPartnerOrder'
+import { TopUpMobile } from './classes/TopUpMobile'
+import { BuyCard } from './classes/BuyCard'
 import { PayBill } from './classes/PayBill'
+import { TopUpGame } from './classes/TopUpGame'
 import { IGetCategory } from './interfaces/requestvtc.interface'
 import { IGetDataSign } from './interfaces/requestvtc.interface'
 import { IVerifyDataSign } from './interfaces/requestvtc.interface'
@@ -21,10 +26,15 @@ export {
   GetCategory,
   GetProduct,
   GetBalance,
-  TopUpMobile,
-  CheckPartnerOrder,
   CheckBillInfo,
+  GetTelcoPromotions,
+  GetCardData,
+  GetCardQuantity,
+  CheckPartnerOrder,
+  TopUpMobile,
+  BuyCard,
   PayBill,
+  TopUpGame,
   // interface
   IGetCategory,
   IGetDataSign,
